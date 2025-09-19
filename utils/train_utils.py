a1198f4183d59178bb0d7cb48cd7ce1d5d257e36
KarthikKolluriKB <karthik.kolluri@essex.ac.uk>
2025-09-19 17:39:54 +0100

    added save_training_config file

--- utils/train_utils.py
+++ utils/train_utils.py
@@ -1,4 +1,8 @@
-from utils.log_config import get_logger
+import os
+import json
+from omegaconf import OmegaConf
+import torch
+from log_config import get_logger
 
 logger = get_logger(log_dir="logs")
 
@@ -27,3 +31,26 @@ def print_module_size(module, module_name: str) -> None:
     logger.info(f"Module: {module_name}")
     total_params = sum(p.numel() for p in module.parameters() if p.requires_grad)
     logger.info(f"{module_name} has {total_params/ 1e6} Million trainable parameters")
+
+def save_training_config(cfg, output_dir):
+    """
+    Saves the training configuration to a JSON file in the specified output directory.
+
+    Args:
+        cfg (omegaconf.DictConfig): The configuration object containing training settings.
+        output_dir (str): The directory where the configuration file will be saved.
+    
+    Returns:
+        str: The path to the saved configuration file.
+    """
+    config_to_save = {
+        "model": OmegaConf.to_container(cfg.get("model", {}), resolve=True),
+        "train": OmegaConf.to_container(cfg.get("train", {}), resolve=True),
+        "data": OmegaConf.to_container(cfg.get("data", {}), resolve=True),
+        "scheduler": OmegaConf.to_container(cfg.get("scheduler", {}), resolve=True),
+        "early_stopping": OmegaConf.to_container(cfg.get("early_stopping", {}), resolve=True),
+    }
+    cfg_out_path = os.path.join(output_dir, "training_config.json")
+    with open(cfg_out_path, "w") as f:
+        json.dump(config_to_save, f, indent=2)
+    return cfg_out_path
