8545ebceb2e04504e320a04fd4697cdafce1854b
KarthikKolluriKB <karthik.kolluri@essex.ac.uk>
2025-09-22 16:09:32 +0100

    added get_lr_scheduler train utils

--- utils/train_utils.py
+++ utils/train_utils.py
@@ -2,8 +2,16 @@ import os
 import json
 from omegaconf import OmegaConf
 import torch
+import transformers
+from transformers import (
+    get_linear_schedule_with_warmup,
+    get_cosine_schedule_with_warmup
+)
+
+# internal imports
 from utils.log_config import get_logger
 
+# Initialize logger
 logger = get_logger(log_dir="logs")
 
 
@@ -54,3 +62,43 @@ def save_training_config(cfg, output_dir):
     with open(cfg_out_path, "w") as f:
         json.dump(config_to_save, f, indent=2)
     return cfg_out_path
+
+
+def get_lr_scheduler(
+        optimizer: torch.optim.Optimizer,
+        scheduler_type: str,
+        num_training_steps: int,
+        num_warmup_steps: int = 0, 
+        num_cycles: float = 0.5,
+        **kwargs,
+    ) -> torch.optim.lr_scheduler.LRScheduler:
+    """
+    Returns a learning rate scheduler based on the specified type.
+
+    Args:
+        optimizer (torch.optim.Optimizer): The optimizer for which to schedule the learning rate.
+        scheduler_type (str): The type of scheduler to use ("linear" or "cosine").
+        num_training_steps (int): Total number of training steps.
+        num_warmup_steps (int, optional): Number of warmup steps. Defaults to 0.
+        num_cycles (float, optional): Number of cycles for cosine scheduler. Defaults to 0.5.
+        **kwargs: Additional keyword arguments for future extensions.
+
+    Returns:
+        transformers.PreTrainedScheduler: The configured learning rate scheduler.
+    """
+    if scheduler_type == "linear_warmup":
+        return get_linear_schedule_with_warmup(
+            optimizer,
+            num_warmup_steps=num_warmup_steps,
+            num_training_steps=num_training_steps
+        )
+    elif scheduler_type == "cosine":
+        return get_cosine_schedule_with_warmup(
+            optimizer,
+            num_warmup_steps=num_warmup_steps,
+            num_training_steps=num_training_steps,
+            num_cycles=num_cycles
+        )
+    else:
+        raise ValueError(f"Unsupported scheduler type: {scheduler_type}")
+    
